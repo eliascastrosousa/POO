1b58db9b5ff1ab71ba0f9663dd7d72e438726173
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2022-03-07 17:03:35 -0300

    commit

---
+++ ListaExJava/src/Exercicio07.java
@@ -0,0 +1,25 @@
+/*7. Escreva um programa que leia dois valores, os quais denominaremos A e B, fornecidos pelo usuário, exibindo respostas
+F ou V para as seguintes questões:
+(a) A é igual a zero?
+(b) B é menor que zero; e
+(c) A OU B são maiores do que zero?
+Use operadores lógicos para formular as expressões necessárias a avaliação dos valores. */
+
+
+import java.util.Scanner;
+
+public class Exercicio07 {
+	
+	public void run() {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Digite um valor para A: ");
+		float a = input.nextFloat();
+		System.out.print("Digite um valor para B: ");
+		float b = input.nextFloat();
+		
+		System.out.println(" a) A Ã© igual a zero? " + ((a == 0) ? "verdadeiro" : "falso"));
+		System.out.println(" b) B Ã© menor que zero?"  + ((b > 0) ? "verdadeiro" : "falso"));
+		System.out.println(" c) A OU B sÃ£o maiores do que zero?" + (((a >0)||(b >0)) ? "verdadeiro" : "falso"));
+	}
+}
--- ListaExJava/src/MainListaEx.java
+++ ListaExJava/src/MainListaEx.java
@@ -31,6 +31,30 @@ public class MainListaEx {
 						Exercicio05 exercicio05 = new Exercicio05();
 						exercicio05.run();
 					} break;
+					case 6: {
+						Exercicio06 exercicio06 = new Exercicio06();
+						exercicio06.run();
+					} break;
+					case 7 :{
+						Exercicio07 exercicio07 = new Exercicio07();
+						exercicio07.run();
+					}break;
+					case  8:{
+						Exercicio08 exercicio08 = new Exercicio08();
+						exercicio08.run();
+					}break;
+					case 9 :{
+						Exercicio09 exercicio09 = new Exercicio09();
+						exercicio09.run();
+					}break;			
+					case 10 :{
+						Exercicio10 exercicio10 = new Exercicio10();
+						exercicio10.run();
+					}break;
+					case 11 :{
+						Exercicio11 exercicio11 = new Exercicio11();
+						exercicio11.run();
+					}break;
 						
 				}		
 			}while(opcao != 0);
