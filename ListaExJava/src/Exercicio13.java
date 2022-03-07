de4174bbe28d08206fc34ac9b38cc3dff69c4705
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2022-03-07 19:40:24 -0300

    commit

--- ListaExJava/src/Exercicio13.java
+++ ListaExJava/src/Exercicio13.java
@@ -1,17 +1,42 @@
+/*
+13. Dados 3 valores reais representando os lados de um possível triângulo, verifique se os mesmos formam um triângulo (a
+medida de qualquer um de seus lados deve ser menor que a soma das medidas dos lados restantes). Caso seja formado um
+triângulo, descubra também se este triângulo é escaleno (três lados diferentes), isósceles (dois lados iguais) ou eqüilátero
+(três lados iguais). 
+*/
 
 import java.util.Scanner;
 
-/*
-3. Leia um nÃºmero qualquer fornecido pelo usuÃ¡rio. Determine se o nÃºmero Ã© maior do que 50, imprimindo uma mensagem
-indicando tal fato.
-*/
 public class Exercicio13 {
 	public void run() {
 		Scanner input = new Scanner(System.in);
-		System.out.print("Digite um valor real: ");
-		float valor = input.nextFloat();
-		if ( valor > 50){
-			System.out.println("O valor lido " + valor + " eh maior que 50!");
+		
+		System.out.print("Digite o valor A: ");
+		float a = input.nextFloat();
+		
+		System.out.print("Digite o valor B: ");
+		float b = input.nextFloat();
+		
+		System.out.print("Digite o valor C: ");
+		float c = input.nextFloat();
+		
+		if( a < b+c && b < a+c && c < a+b) {
+			String triangulo;
+			
+			if((a==b)&& (b==c)&&(c==a)) {
+				triangulo = "equilatero";
+			}
+			if((a==b)!= (b==c)!=(c==a)) {
+				triangulo = "escaleno";
+			}
+			else {
+				triangulo = "isoceles";
+			}
+			System.out.print("Este conjunto de medidas podem formar um triangulo " + triangulo);
+			
+		}else {
+			System.out.print("Este conjunto de medidas NÃO PODEM FORMAR um triangulo.");
+		
 		}
 	}
 }
--- ListaExJava/src/Exercicio14.java
+++ ListaExJava/src/Exercicio14.java
@@ -1,15 +1,36 @@
+/*Escreva um programa que leia 3 notas (valores reais), calculando e exibindo sua média aritmética. Imprima também
+"Aprovado" se a média for maior que 7, "Reprovado" se for menor que 3 e "Exame" se estiver entre 3 e 7. 
+*/
 
 import java.util.Scanner;
 
 public class Exercicio14 {
 	public void run() {
 		Scanner input = new Scanner(System.in);
-		System.out.print("Digite um valor real: ");
-		float valor = input.nextFloat();
-		if ( valor < 100){
-			System.out.println("O valor lido " + valor + " eh menor que 100!");
-		}else {
-			System.out.println("O valor lido " + valor + " eh maior ou igual a 100!");
+		
+		System.out.print("Digite a 1a nota: ");
+		float a = input.nextFloat();
+		System.out.print("Digite a 2a nota: ");
+		float b = input.nextFloat();
+		System.out.print("Digite a 3a nota: ");
+		float c = input.nextFloat();
+		
+		float media;
+		media = (a + b + c) / 3; 
+		
+		if(media>7){
+			System.out.println("Nota: "+ media);
+			System.out.println("Aluno Aprovado!");
 		}
+		 else if(media>3 && media<7) {
+			System.out.println("Nota: "+ media);
+			System.out.println("Está de Exame!");
+		}
+		else if(media<3) {
+			System.out.println("Nota: "+ media);
+			System.out.println("Reprovado!");
+		}
+		
+		
 	}
 }
