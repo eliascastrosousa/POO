9e6ee7729a55aaaa2b1c24a01360e7b33fbc1610
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2022-03-10 16:41:54 -0300

    commit

--- ListaExJava/src/Exercicio18.java
+++ ListaExJava/src/Exercicio18.java
@@ -10,20 +10,44 @@ import java.util.Iterator;
 
 public class Exercicio18 {
 	
-	 String Numeros[][] = {  
-	          {"Zero", "Um", "Dois", "Três", "Quatro", "Cinco", "Seis", "Sete", "Oito", "Nove", "Dez",  
-	          "Onze", "Doze", "Treze", "Quatorze", "Quinze", "Desesseis", "Desessete", "Dezoito", "Desenove"},  
-	          {"Vinte", "Trinta", "Quarenta", "Cinquenta", "Sessenta", "Setenta", "Oitenta", "Noventa"},  
-	          {"Cem", "Cento", "Duzentos", "Trezentos", "Quatrocentos", "Quinhentos", "Seiscentos",  
-	          "Setecentos", "Oitocentos", "Novecentos"}  
-	          };  
+	int aux, aux2;
+	
+	 String Numeros[] = {"Zero", "Um", "Dois", "Três", "Quatro", "Cinco", "Seis", "Sete", "Oito", "Nove", "Dez",  
+	          				"Onze", "Doze", "Treze", "Quatorze", "Quinze", "Desesseis", "Desessete", "Dezoito", "Desenove"};
+	 
+	 String Dezenas[]= {"Dez","Vinte", "Trinta", "Quarenta", "Cinquenta", "Sessenta", "Setenta", "Oitenta", "Noventa"};
+	 
+	 String Centenas[] =  {"Cem" } ;
+	          
 
 	public void run() {
 		Scanner input = new Scanner(System.in);
 		
 		System.out.print("Digite um número inteiro entre 0 e 100: ");
 		int a = input.nextInt();
-				
+		
+		
+		for(int c = 0; c <= a; c++) {
+			aux = c;        
+		}
+		
+		if(a > 20 && a < 100) {
+					      
+	        String number = String.valueOf(a);
+
+	        char[] digits1 = number.toCharArray();
+
+	        for(int i = 0; i < digits1.length; i++) {
+	            
+	        	System.out.println(digits1[i]);
+	        	
+	            System.out.println(Dezenas[i]);	
+	            
+	            
+	        }
+		}
+		
+			//System.out.println(Numeros[aux]);
 		
 		
 	}
